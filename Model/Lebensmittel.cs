074fc4536d7b42e20a5e9fbf8905196ff478dd62
Lukas Groepl <GroeplLukas@dv-schulen.de>
2018-06-07 15:23:01 +0200

    hopefully itll  be there

---
+++ Model/Lebensmittel.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Lebensmittel
+    {
+        private double essbarkeit = 100;
+        private double verderb;
+        private string bezeichnung;
+        private double idealtemperatur;
+        private double multiplikator;
+        private double gefrierpunkt;
+
+        public Lebensmittel(string bezeichnung, double verderb, double idealtemperatur, double multiplikator, double gefrierpunkt)
+        {
+            Bezeichnung = bezeichnung;
+            Verderb = verderb;
+            Idealtemperatur = idealtemperatur;
+            Multiplikator = multiplikator;
+            Gefrierpunkt = gefrierpunkt;
+        }
+
+
+        public double Essbarkeit
+        {
+            get
+            {
+                return essbarkeit;
+            }
+
+            set
+            {
+                essbarkeit = value;
+            }
+        }
+
+        public double Verderb
+        {
+            get
+            {
+                return verderb;
+            }
+
+            set
+            {
+                verderb = value;
+            }
+        }
+
+        public string Bezeichnung
+        {
+            get
+            {
+                return bezeichnung;
+            }
+
+            set
+            {
+                bezeichnung = value;
+            }
+        }
+
+        public double Idealtemperatur
+        {
+            get
+            {
+                return idealtemperatur;
+            }
+
+            set
+            {
+                idealtemperatur = value;
+            }
+        }
+
+        public double Multiplikator
+        {
+            get
+            {
+                return multiplikator;
+            }
+
+            set
+            {
+                multiplikator = value;
+            }
+        }
+
+        public double Gefrierpunkt
+        {
+            get
+            {
+                return gefrierpunkt;
+            }
+
+            set
+            {
+                gefrierpunkt = value;
+            }
+        }
+    }
+}
---
+++ ViewModel/VMLebensmittel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+namespace ViewModel
+{
+    public class VMLebensmittel
+    {
+        private Lebensmittel lebensmittel;
+
+        public VMLebensmittel(string bezeichnung, double verderb, double idealtemperatur, double multiplikator, double gefrierpunkt)
+        {
+            lebensmittel = new Lebensmittel(bezeichnung, verderb, idealtemperatur, multiplikator, gefrierpunkt);
+        }
+    }
+}
